3e0b233fc1cd53a1a661838f3452f3325736e2db
eunuintelegnimic <arteom.tru@gmail.com>
2025-05-05 17:35:15 +0300

    Edited the way we handle window size and position

--- main.c
+++ main.c
@@ -1,31 +1,37 @@
 #include <raylib.h>
 #include <raymath.h>
 
+
 int main()
 {
 //  --- Transparency need to be set beefore window initialization
     SetConfigFlags(FLAG_WINDOW_TRANSPARENT);
+
 //  --- Window size is set to default value (screen size)
     InitWindow(0, 0, "weyes");
+
 //  --- Keep above other
     SetWindowState(FLAG_WINDOW_TOPMOST);
+    SetWindowState(FLAG_WINDOW_MOUSE_PASSTHROUGH);
+    SetWindowState(FLAG_WINDOW_UNDECORATED);
 
-//  --- Set window size to 4/5 of the screen size
-    int windowWidth = GetMonitorWidth(0) / 5;
-    int windowHeight = GetMonitorHeight(0) / 4;
+//  --- Set eyes size to 4/5 of the screen size
+    int windowWidth = GetMonitorWidth(0);
+    int windowHeight = GetMonitorHeight(0);
+
+    int eyesWidth = windowWidth / 5;
+    int eyesHeight = windowHeight / 4;
+
 //      If the screen is too small set specific values
-    if (GetScreenWidth() <= 1280 || GetScreenHeight() <= 720){
-        windowWidth = 256;
-        windowHeight = 180;
-    } SetWindowSize(windowWidth, windowHeight);
-//  --- Place it in bottom-right corner
-    SetWindowPosition(GetMonitorWidth(0) - windowWidth,
-                      GetMonitorHeight(0) - windowHeight);
+    if (windowWidth <= 1280 || windowHeight <= 720){
+        eyesWidth = 256;
+        eyesHeight = 180;
+    }
 
 //  --- Eyes sizes and positions and pupils radius
-    Vector2 eyeSize = {windowWidth/4, windowHeight/2};
-    Vector2 eyeL = {windowWidth/4, windowHeight/2};
-    Vector2 eyeR = {windowWidth - windowWidth/4, windowHeight/2};
+    Vector2 eyeSize = {eyesWidth/4, eyesHeight/2};
+    Vector2 eyeL = {windowWidth - eyesWidth/4, windowHeight - eyesHeight/2};
+    Vector2 eyeR = {windowWidth - (eyesWidth - eyesWidth/4), windowHeight - eyesHeight/2};
 
     Vector2 pupilL = eyeL;
     Vector2 pupilR = eyeR;
